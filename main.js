3ab78b31da179763df989b7ada93bc98c5c66060
Stephen Hornbeek <hbeeken@hotmail.com>
2018-11-05 17:25:30 -0800

    first upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    //feed to parse
+    var feed = "http://feeds.feedburner.com/raymondcamdensblog?format=xml";
+    
+    $.ajax(feed, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+    
+
+        }   
+    });
+    
+});
